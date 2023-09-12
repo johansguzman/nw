63bb5b5c97daa6e1aaf7a72a9dcaf4e301a546b5
Esteban <johansguzman08@gmail.com>
2023-09-12 15:44:53 -0300

    subiendo archivos importados johans guzman 3-D

---
+++ tiempo.py
@@ -0,0 +1,10 @@
+def segundo_a_minutos(segundo):
+    return segundo /60
+
+def minutos_a_horas(minutos):
+    return minutos /60
+
+if __name__=="__name__":
+    segundo= 150
+    minutos= segundo_a_minutos(segundo)
+    print(f"{segundo} segundo son equivalente a {minutos}")
